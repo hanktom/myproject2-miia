d14aac3b8cc0be14bde54e53c54d5004161c156a
Hank Tom <hanktom@gmail.com>
2022-03-04 15:27:08 +0800

    Scoring old way

---
+++ src/main/java/com/tom/ScoringNG.java
@@ -0,0 +1,14 @@
+package com.tom;
+
+public class ScoringNG {
+    public static void main(String[] args) {
+        String[] names = {"Jack", "Hank", "Karen", "Elva", "Benny"};
+        int[] englishs = {70, 80, 90, 60, 40};
+        int[] maths = {88, 97, 54, 32, 75};
+        for (int i = 0; i < 5; i++) {
+            if (i != 3) {
+                System.out.println(names[i] + "\t" + englishs[i] + "\t" + maths[i] + "\t" + (englishs[i] + maths[i]) / 2);
+            }
+        }
+    }
+}
