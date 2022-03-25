7bd51191d86b583b9b4dcd4aa98b30444f38c1d3
Hank Tom <hanktom@gmail.com>
2022-03-25 16:23:05 +0800

    Station class design, why?

--- src/main/java/com/tom/ticket/Tester.java
+++ src/main/java/com/tom/ticket/Tester.java
@@ -1,11 +1,15 @@
 package com.tom.ticket;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
         //Constructor, constant value
         Ticket ticket = new Ticket(
-                Ticket.TAIPEI_STATION,
-                Ticket.KAOHSIUNG_STATION);
+                Station.TAIPEI_STATION,
+                Station.KAOHSIUNG_STATION);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your start station?(1: Taipei, 2: Taichung, 3: Kaohsiung)");
 
     }
 }
