9e2e8b9744ce61ee3bc593d06607f6cbc890c77d
Hank Tom <hanktom@gmail.com>
2022-04-01 16:25:58 +0800

    Polymorphism with collection

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -1,7 +1,13 @@
 package com.tom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello {
     public static void main(String[] args) {
+
+
+
         int diff = Math.abs(100-300);
         System.out.println(diff);
 
--- src/main/java/com/tom/sales/SalesTester.java
+++ src/main/java/com/tom/sales/SalesTester.java
@@ -1,4 +1,8 @@
 package com.tom.sales;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //  一般客戶 - 滿千送百
 //  銀級客戶 - 滿千送百, 再送百元還元金
 //  金級客戶 - 滿千送二百，再送二百還元金
@@ -13,15 +17,17 @@ package com.tom.sales;
  */
 public class SalesTester {
     public static void main(String[] args) {
-        Customer c1 = new Customer("0001", 1200);
-        Customer c2 = new Customer("0002", 800);
-        SilverCustomer c3 = new SilverCustomer("0003", 2000);
-        GoldenCustomer c4 = new GoldenCustomer("0004", 5000);
-        DiscountCustomer c5 = new DiscountCustomer("0006", 900);
-        c1.print();
-        c2.print();
-        c3.print();
-        c4.print();
-        c5.print();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("0001", 1200));
+        customers.add(new Customer("0002", 800));
+        customers.add(new SilverCustomer("0003", 2000));
+        customers.add(new GoldenCustomer("0004", 5000));
+        customers.add(new DiscountCustomer("0006", 900));
+        for (int i = 0; i < 5; i++) {
+            customers.get(i).print();
+        }
+        for (Customer c: customers) {
+            c.print();
+        }
     }
 }
